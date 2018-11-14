0a1c7d4dee619da61780480601c2e279bc82abf5
Eleanor <Eleanor.sunchen@gmail.com>
2018-11-14 10:35:14 +0800

    fix LeapYear.java

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,4 +1,44 @@
 /** Class that determines whether or not a year is a leap year.
+ *  Eleanor YOUR NAME HERE
+ */
+/*public class LeapYear {
+
+    private static boolean isLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /** Calls isLeapYear to print correct statement.
+     *  @param  year to be analyzed
+     */
+   /* private static void checkLeapYear(int year) {
+        if (isLeapYear(year)) {
+            System.out.printf("%d is a leap year.\n", year);
+        } else {
+            System.out.printf("%d is not a leap year.\n", year);
+        }
+    }
+
+    /** Must be provided an integer as a command line argument ARGS. */
+    /*public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Please enter command line arguments.");
+            System.out.println("e.g. java Year 2000");
+        }
+        for (int i = 0; i < args.length; i++) {
+            try {
+                int year = Integer.parseInt(args[i]);
+                checkLeapYear(year);
+            } catch (NumberFormatException e) {
+                System.out.printf("%s is not a valid number.\n", args[i]);
+            }
+        }
+    }
+}
+/** Class that determines whether or not a year is a leap year.
  *  @author YOUR NAME HERE
  */
 public class LeapYear {
@@ -12,6 +52,14 @@ public class LeapYear {
         } else {
             System.out.printf("%d is not a leap year.\n", year);
         }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if(( (year % 4 == 0)&&(year % 100 != 0)) || (year % 400 == 0  )){
+            return true;
+        }
+
+        return false;
     }
 
     /** Must be provided an integer as a command line argument ARGS. */
@@ -30,4 +78,3 @@ public class LeapYear {
         }
     }
 }
-
