2b27c8b8a00ef70886923b430bdb11275e053c3f
Eleanor <Eleanor.sunchen@gmail.com>
2018-11-15 18:05:47 +0800

    tested netforce method

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -6,6 +6,7 @@ public class Planet {
 	public double yyVel;
 	public double mass;
 	public String imgFileName;
+	public static double G = 6.67e-11;
 
 	public Planet(double xP, double yP, double xV, double yV, double m, String img) {
 		xxPos = xP;
@@ -26,8 +27,42 @@ public class Planet {
 	}
 
 	public double calcDistance(Planet p) {
-
-		return Math.sqrt((p.xxPos-this.xxPos)*(p.xxPos-this.xxPos)+(p.yyPos-this.yyPos)*(p.yyPos-this.yyPos));
-
+		return Math.sqrt((p.xxPos - this.xxPos)*(p.xxPos - this.xxPos) + (p.yyPos - this.yyPos) * (p.yyPos - this.yyPos));
 	}
+
+	public double calcForceExertedBy(Planet p) {
+		return G * this.mass * p.mass / this.calcDistance(p) / this.calcDistance(p);
+	}
+
+	public double calcForceExertedByX(Planet p) {
+		return this.calcForceExertedBy(p) / this.calcDistance(p) * (p.xxPos - this.xxPos);
+	}
+
+	public double calcForceExertedByY(Planet p) {
+		return this.calcForceExertedBy(p) / this.calcDistance(p) * (p.yyPos - this. yyPos);
+	}
+
+	public double calcNetForceExertedByX(Planet[] a) {
+		double sum = 0;
+		for (int i = 0; i < a.length; i ++ ) {
+			if (this.equals(a[i])) {
+				continue;
+			}
+			sum = sum + this.calcForceExertedByX(a[i]);
+		}
+		return sum;
+	}
+
+	public double calcNetForceExertedByY(Planet[] b) {
+		double sum = 0;
+		for (int i = 0; i < b.length; i ++ ) {
+			if (this.equals(b[i])) {
+				continue;
+			}
+			sum = sum + this.calcForceExertedByY(b[i]);
+		}
+		return sum;
+	}
+
+
 }
